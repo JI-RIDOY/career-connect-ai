1abdd87ff4f88cb2a0faa427f8451118ce4ec376
JI-RIDOY <aridoy102536@gmail.com>
2025-12-10 12:41:00 +0600

    job done

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -10,6 +10,13 @@ import ATSScoreCheck from "../pages/ATSScoreCheck";
 import Settings from "../pages/Settings";
 import MockInterview from "../pages/MockInterview";
 import LearningPathGenerator from "../pages/LearningPathGenerator";
+import Jobs from "../pages/Jobs";
+import JobDetail from "../pages/JobDetail";
+import PostJob from "../pages/PostJob";
+import MyJobs from "../pages/MyJobs";
+import MyApplications from "../pages/MyApplications";
+import JobApplications from "../pages/JobApplications";
+import EditJob from "../pages/EditJob";
 
 export const router = createBrowserRouter([
   {
@@ -43,6 +50,38 @@ export const router = createBrowserRouter([
         {
           path: '/learning-path',
           Component: LearningPathGenerator
+        },
+        {
+          path: '/jobs',
+          Component: Jobs
+        },
+        {
+          path: '/job/:id',
+          Component: JobDetail
+        },
+        {
+          path: '/post-job',
+          Component: PostJob,
+        },
+        {
+          path: '/my-jobs',
+          Component: MyJobs,
+        },
+        {
+          path: '/my-applications',
+          Component: MyApplications,
+        },
+        {
+          path: '/apply/:id',
+          Component: JobDetail
+        },
+        {
+          path: '/job/:id/applications',
+          Component: JobApplications,
+        },
+        {
+          path: '/edit-job/:id',
+          Component: EditJob
         }
     ]
   },
